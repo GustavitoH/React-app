38a72d3147dd67b447c90757f17ba59912569537
gustavo hidalgo <gus-tavo99@outlook.es>
2021-11-05 18:26:30 -0500

    Obteniendo values

---
+++ react-app/src/Components/Users/Formulario.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react"
+import { Button, TextField, Box } from "@mui/material";
+
+
+function Formulario() {
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState();
+    const [edad, setEdad] = useState();
+    const [ciudad, setCiudad] = useState();
+
+    const notificar = () => {
+        alert(nombre)
+    }
+    return (
+       <div>
+            <Box component="form"
+            sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+            }}>
+                <TextField id="fullWidth" label="Nombre"size="small" margin="dense" value={nombre} onChange={(e) =>setNombre(e.target.value)}/>
+                <TextField id="fullWidth" label="Apellido"size="small" margin="dense" value={apellido}/>
+            </Box>
+            <Box component="form"
+            sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+            }}>
+                <TextField id="fullWidth" label="Edad"size="small" margin="dense" type="number" value={edad}/>
+                <TextField id="fullWidth" label="Ciudad"size="small"  value={ciudad}/>
+            </Box>
+            <Box component="form"
+            sx={{
+                '& > :not(style)': { m: 1, width: '56ch' },
+            }}>
+            <Button fullWidth  variant="contained" color="primary" onClick={notificar}>Crear</Button>
+            </Box>
+
+       </div>
+    )
+}
+
+export default Formulario;
